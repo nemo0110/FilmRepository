2c6be32be7084e25f4eb186c09b3d2acb7306160
ContentoM <m.contento@accademiainformatica.com>
2020-09-25 17:12:06 +0200

    genere

---
+++ Film/src/main/java/com/example/film/controller/GenereController.java
@@ -0,0 +1,28 @@
+package com.example.film.controller;
+
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.film.model.Genere;
+import com.example.film.repository.RepGenere;
+
+@CrossOrigin("*")
+@RestController
+public class GenereController {
+
+@Autowired
+RepGenere repG;
+
+@RequestMapping(value= {"/sel"})
+public List<Genere> findGeneri() {
+	List<Genere> listaGeneri = repG.findGen();
+	System.out.println(listaGeneri);
+	return listaGeneri;
+}
+}
--- Film/src/main/java/com/example/film/model/Genere.java
+++ Film/src/main/java/com/example/film/model/Genere.java
@@ -14,6 +14,19 @@ public class Genere {
     private Long id;     
 	private String descrizione;
 	
+	public Genere(Long id) {
+		this.id = id;
+	}
+	public Genere(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	public Genere(Long id, String descrizione) {
+		super();
+		this.id = id;
+		this.descrizione = descrizione;
+	}
+	public Genere() {}
+	
 	public Long getId() {
 		return id;
 	}
